085c19ab7f7d5d5fc543224facc78e3e1405a8e9
aravinthmathav <aravinthmathav@gmail.com>
2021-10-31 16:32:18 +0530

    first

---
+++ src/org/sample/Company.java
@@ -0,0 +1,16 @@
+package org.sample;
+
+public class Company {
+	
+public static void main(String[]args) {
+	for(int i=0;i<10;i++) {
+		
+		if(i==2) {System.exit(0);
+			
+		}
+		System.out.println(i);
+	}
+	
+}
+
+}
